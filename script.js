301362706ad747120c4d30084165db66d7a7f4ce
Mezzky <ryansahadha@gmail.com>
2023-02-04 23:47:09 +0800

    Add: Feature and Contact Page

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const hamburger = document.querySelector('#hamburger');
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector("header .navbar");
 const ul = document.querySelector("header .navbar ul");
-// const header = document.querySelector('header');
+const appraisal = document.getElementById("appraisal");
+const about = document.getElementById("about");
+const line = document.getElementById("line");
+const textApr = document.getElementById("textApraisal");
+const textAbo = document.getElementById("textAbout");
+const back = document.querySelectorAll("#back");
 
 // Hamburger
 hamburger.addEventListener('click', () => {
@@ -14,4 +19,31 @@ hamburger.addEventListener('click', () => {
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('slide');
     ul.classList.toggle('slide');
+    textApr.classList.remove('show');
+    textAbo.classList.remove('show');
+    line.classList.remove('show');
+});
+
+// Multi List Link
+appraisal.addEventListener('click', () => {
+    textApr.classList.add('show');
+    line.classList.add('show');
+    ul.classList.add('show');
+    textAbo.classList.remove('show');
+});
+
+about.addEventListener('click', () => {
+    textAbo.classList.add('show');
+    line.classList.add('show');
+    ul.classList.add('show');
+    textApr.classList.remove('show');
+});
+
+back.forEach(el => {
+    el.addEventListener('click', () => {
+        textApr.classList.remove('show');
+        textAbo.classList.remove('show');
+        line.classList.remove('show');
+        ul.classList.remove('show');
+    });
 });
